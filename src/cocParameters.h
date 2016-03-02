044b266b2eee2fab5e3fcd61a5f2423e76eb3e36
Rene Christen <accounts@renechristen.net>
2016-03-02 13:05:46 +1100

    first commit

---
+++ src/cocParameters.h
@@ -0,0 +1,113 @@
+#pragma once
+
+#include "cinder/Xml.h"
+#include "cinder/Log.h"
+#include "cinder/Log.h"
+
+#include <fstream>
+
+namespace coc {
+
+enum parameterType {
+	PARAM_FLOAT = 0,
+	PARAM_INT,
+	PARAM_BOOL
+};
+
+struct Parameter {
+
+	std::string 	name;
+	parameterType 	type;
+	void*			val;
+
+};
+
+static std::vector<Parameter> parameters;
+
+template<typename T>
+void registerParameter( std::string _name, T *_val )
+{
+	parameters.push_back(Parameter());
+	parameters.back().name = _name;
+	parameters.back().val = _val;
+	if (std::is_same<T, float>::value) parameters.back().type = PARAM_FLOAT;
+	else if (std::is_same<T, int>::value) parameters.back().type = PARAM_INT;
+	else if (std::is_same<T, bool>::value) parameters.back().type = PARAM_BOOL;
+
+}
+
+static void saveParameters( std::string _filename = "config_params.xml" )
+{
+
+	ci::XmlTree xml("parameters","");
+	for (int i=0; i<parameters.size(); i++) {
+
+		ci::XmlTree p( "parameter", "" );
+		p.setAttribute( "name", parameters[i].name );
+		p.setAttribute( "type", (int) parameters[i].type );
+
+		switch (parameters[i].type) {
+			case PARAM_FLOAT:
+				p.setAttribute( "val", *(float*) parameters[i].val );
+				break;
+			case PARAM_INT:
+				p.setAttribute( "val", *(int*) parameters[i].val );
+				break;
+			case PARAM_BOOL:
+				p.setAttribute( "val", (int) *(bool*) parameters[i].val );
+				break;
+		}
+
+		xml.push_back( p );
+	}
+
+	xml.write( ci::writeFile( ci::app::getAssetPath( _filename ) ) );
+
+	CI_LOG_I( "Saved GUI" );
+
+}
+
+static void loadParameters( std::string _filename = "config_params.xml" )
+{
+
+	ci::XmlTree xml;
+	try {
+		xml = ci::XmlTree( ci::app::loadAsset( _filename ) );
+	}
+	catch( ci::Exception exc ){
+		CI_LOG_EXCEPTION( "Loading XML failed, creating file", exc )
+		std::ofstream file( (ci::app::getAssetPath("") / _filename).string() );
+		file.close();
+	}
+
+
+	for( auto parameter = xml.begin( "parameters/parameter" ); parameter != xml.end(); parameter ++ ) {
+
+		std::string 	name = parameter->getAttributeValue<std::string>("name","");
+		parameterType 	type = (parameterType) parameter->getAttributeValue<int>( "type", 0 );
+
+		for (int i=0; i<parameters.size(); i++) {
+
+			if ( name == parameters[i].name ) {
+
+				if (type != parameters[i].type) CI_LOG_E( name << " types do not match!");
+
+				switch ( type ) {
+					case PARAM_FLOAT:
+						*(float *) parameters[i].val = parameter->getAttributeValue<float>( "val", 0.0f );
+						break;
+					case PARAM_INT:
+						*(int *) parameters[i].val = parameter->getAttributeValue<int>( "val", 0.0f );
+						break;
+					case PARAM_BOOL:
+						*(bool *) parameters[i].val = (bool) parameter->getAttributeValue<int>( "val", 0 );
+						break;
+				}
+
+			}
+		}
+	}
+
+}
+
+}//namespace coc
